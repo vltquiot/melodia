ca1c6cb9105228eb4f943d2d8bb90fefbe7bb22d
Vltquiot <valentin.quiot@gmail.com>
2025-10-02 00:54:08 +0200

    Add the prompt for QA text file

--- launch_venv.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-color 0A
-echo 			==============================
-echo 			=======To run the code =======
-echo 			==source .venv/bin/activate===
-echo 			==============================
-wsl
